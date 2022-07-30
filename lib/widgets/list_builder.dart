65e689205a9fd4e00a8c919bb48fad47fcbe3551
Amit <amit@3xpo.com>
2022-07-30 20:36:53 +1000

    improve product listview: list loading state

--- lib/widgets/list_builder.dart
+++ lib/widgets/list_builder.dart
@@ -15,52 +15,64 @@ class _ListBuilder extends State<ListBuilder> {
     return FutureBuilder(
         future: widget.datalist,
         builder: (BuildContext context, AsyncSnapshot snapshot) {
-          if (snapshot.data == null) {
-            return const Center(child: Text("Loading . . ."));
+          if (snapshot.hasData) {
+            return snapshot.data.length > 0
+                ? ListView.separated(
+                    shrinkWrap: true,
+                    physics: const ScrollPhysics(),
+                    padding: const EdgeInsets.all(8),
+                    itemCount: snapshot.data.length,
+                    separatorBuilder: (_, __) => const Divider(height: 1),
+                    itemBuilder: (BuildContext context, int index) {
+                      return Slidable(
+                          groupTag: "1",
+                          key: const ValueKey(0),
+                          endActionPane: const ActionPane(
+                            motion: ScrollMotion(),
+                            children: [
+                              SlidableAction(
+                                // An action can be bigger than the others.
+                                onPressed: doNothing,
+                                backgroundColor: Color(0xFF7BC043),
+                                foregroundColor: Colors.white,
+                                icon: Icons.archive,
+                                label: 'Archive',
+                              ),
+                              SlidableAction(
+                                onPressed: doNothing,
+                                backgroundColor: Color(0xFF0392CF),
+                                foregroundColor: Colors.white,
+                                icon: Icons.save,
+                                label: 'Save',
+                              ),
+                            ],
+                          ),
+                          child: InkWell(
+                              onTap: () {},
+                              splashFactory: InkRipple.splashFactory,
+                              child: ListTile(
+                                leading: const CircleAvatar(
+                                    backgroundImage: NetworkImage(
+                                        "https://source.unsplash.com/random")),
+                                title: Text(snapshot.data[index]["title"]),
+                                subtitle:
+                                    Text(snapshot.data[index]["subtitle"]),
+                                isThreeLine: true,
+                                dense: true,
+                              )));
+                    })
+                : const Center(
+                    child: Text("No items available"),
+                  );
+          } else if (snapshot.hasError) {
+            return const Center(
+              child: Text("Something went wrong..."),
+            );
+          } else {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
           }
-          return ListView.separated(
-              shrinkWrap: true,
-              physics: const ScrollPhysics(),
-              padding: const EdgeInsets.all(8),
-              itemCount: snapshot.data.length,
-              separatorBuilder: (_, __) => const Divider(height: 1),
-              itemBuilder: (BuildContext context, int index) {
-                return Slidable(
-                    groupTag: "1",
-                    key: const ValueKey(0),
-                    endActionPane: const ActionPane(
-                      motion: ScrollMotion(),
-                      children: [
-                        SlidableAction(
-                          // An action can be bigger than the others.
-                          onPressed: doNothing,
-                          backgroundColor: Color(0xFF7BC043),
-                          foregroundColor: Colors.white,
-                          icon: Icons.archive,
-                          label: 'Archive',
-                        ),
-                        SlidableAction(
-                          onPressed: doNothing,
-                          backgroundColor: Color(0xFF0392CF),
-                          foregroundColor: Colors.white,
-                          icon: Icons.save,
-                          label: 'Save',
-                        ),
-                      ],
-                    ),
-                    child: InkWell(
-                        onTap: () {},
-                        splashFactory: InkRipple.splashFactory,
-                        child: ListTile(
-                          leading: const CircleAvatar(
-                              backgroundImage: NetworkImage(
-                                  "https://source.unsplash.com/random")),
-                          title: Text(snapshot.data[index]["title"]),
-                          subtitle: Text(snapshot.data[index]["subtitle"]),
-                          isThreeLine: true,
-                          dense: true,
-                        )));
-              });
         });
   }
 }
